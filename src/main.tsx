e17d8bbb795950b25af708e25b92d21d6ce45bd8
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-08-07 03:44:20 +0300

    toastify seconds updated

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
-    <ToastContainer position='top-center' />
+    <ToastContainer position='top-center' autoClose={2250} />
   </Provider>
 );
